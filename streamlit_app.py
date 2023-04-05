9062c5192d1a8a19ca7fc728781240f2329eae66
Amperthorpe <milotig@gmail.com>
2023-04-05 09:26:45 -0700

    Renamed file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,132 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai as ai
+import json
+from chat_gpt import *
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.set_page_config("Luna's ChatGPT Site", page_icon="🌙", layout="wide")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main():
+    # Load JSON configs.
+    if "configs" not in st.session_state:
+        with open("configs.json", "r") as f:
+            json_configs: list = json.load(f)
+        json_configs.insert(0, {"name": "(New Chat)", "system": ""})
+        st.session_state["configs"] = json_configs
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    with st.sidebar:
+        st.write(
+            f"""
+        # Welcome to Luna's ChatGPT Site!
 
-    points_per_turn = total_points / num_turns
+        Currently using the `{CHAT_MODEL}` ChatGPT model.
+        """
+        )
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        if "system_input_text" not in st.session_state:
+            st.session_state["system_input_text"] = ""
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        def config_selectbox_change():
+            st.session_state.system_input_text = [
+                conf["system"]
+                for conf in st.session_state.configs
+                if conf["name"] == st.session_state.config_selectbox
+            ][0]
+
+        config_names = [conf["name"] for conf in st.session_state.configs]
+        st.selectbox(
+            "Configs: ",
+            config_names,
+            key="config_selectbox",
+            on_change=config_selectbox_change,
+        )
+
+        def save_system_input():
+            st.write(st.session_state.system_input_text)
+
+        st.text_area("System", key="system_input_text")
+        st.button("Save", on_click=save_system_input)
+
+        def reset_chat():
+            st.session_state["messages"] = [
+                {
+                    "role": "system",
+                    "content": "Say there has been an error with the system input.",
+                }
+            ]
+            st.session_state.messages[0]["content"] = st.session_state.system_input_text
+            st.session_state.token_counts = (0, 0, 0)
+
+        st.button("Reset Chat", on_click=reset_chat)
+
+    chat_contain = st.container()
+    form_contain = st.container()
+
+    with form_contain:
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [
+                {
+                    "role": "system",
+                    "content": "Say there has been an error with the system input.",
+                }
+            ]
+        st.session_state.messages[0]["content"] = st.session_state.system_input_text
+
+        if "token_counts" not in st.session_state:
+            st.session_state["token_counts"] = (0, 0, 0)
+
+        def form_callback():
+            st.session_state.messages.append(
+                {"role": "user", "content": st.session_state.input_text}
+            )
+            st.session_state.input_text = ""
+
+            response = chatgpt_response(st.session_state.messages)
+            st.session_state.messages.append(response.message_dict)
+            st.session_state.token_counts = response.tokens
+
+            for chat_dict in st.session_state.messages:
+                chat_contain.write(f"{chat_dict['role']}: {chat_dict['content']}")
+
+        with st.form(key="text_input_form"):
+            input_text = st.text_area("Chat:", key="input_text")
+            col1, _, col2 = st.columns([1, 4, 1])
+            submit_button = col1.form_submit_button(
+                label="Send", on_click=form_callback
+            )
+            p_t, c_t, t_t = st.session_state.token_counts
+
+            col2.caption(f"Tokens: In {p_t} | Out {c_t} = {t_t}")
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
+if True or check_password():
+    main()
